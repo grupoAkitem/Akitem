302e71741e3a9a3a58a3ff59bdb605e4f27b23e7
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-04-15 07:03:04 -0300

    adiciona cards de produtos

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 const allCategories = document.getElementById('all-categories');
+const inputSearch = document.getElementById('search');
+const sectionCardProducts = document.getElementById('cardProducts');
 
 // function para fazer as requisição das APIs
-async function getCategories(req, product, categorie) {
-    let url = 'https://api.mercadolibre.com/sites/MLB/categories';
+// site da documentação da API https://developers.mercadolivre.com.br/pt_br/itens-e-buscas
+async function getAPIs(req, product, categorie) {
+    let url = '';
     switch (req) {
         case 'productGeneral':
-            url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`
+            url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+            break;
         case 'productCategorie':
-            url =  `https://api.mercadolibre.com/sites/MLB/search?category=${categorie}`
+            url =  `https://api.mercadolibre.com/sites/MLB/search?category=${categorie}`;
+            break;
         case 'productGeneralCategorie':
-            url = ` https://api.mercadolibre.com/sites/MLB/search?category=${categorie}&q=${product}`
+            url = ` https://api.mercadolibre.com/sites/MLB/search?category=${categorie}&q=${product}`;
+            break;
         default:
             url = 'https://api.mercadolibre.com/sites/MLB/categories';
+            break;
     }
     const response = await fetch(url);
     const result = await response.json();
     return result;
   }
 
+  const loading = (bol) => {
+    if (bol) {
+      const container = document.querySelector('.container');
+      const h1 = document.createElement('h1');
+      h1.className = 'loading';
+      h1.innerHTML = 'CARREGANDO...';
+      container.appendChild(h1);
+    } else {
+      const apagaCarregar = document.querySelector('.loading');
+      apagaCarregar.parentNode.removeChild(apagaCarregar);
+    }
+  };
 
   // function para criar o select das categorias
     const selectCategories = async () => {
-    const categories = await getCategories();
+    const categories = await getAPIs();
     categories.forEach((element) => {
         const optionsCategories = document.createElement('option');
         optionsCategories.value = element.id;
@@ -47,22 +66,29 @@ async function getCategories(req, product, categorie) {
   }
   
   // function para criar section com elementos
-  function createProductItemElement({ sku, name, image }) {
+  function createProductItemElement({ id, title, thumbnail }) {
     const section = document.createElement('section');
     section.className = 'item';
   
-    section.appendChild(createCustomElement('span', 'item__sku', sku));
-    section.appendChild(createCustomElement('span', 'item__title', name));
-    section.appendChild(createProductImageElement(image));
+    section.appendChild(createCustomElement('span', 'item__id', id));
+    section.appendChild(createCustomElement('span', 'item__title', title));
+    section.appendChild(createProductImageElement(thumbnail));
     section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   
     return section;
   }
 
-//   const createCardsProducts = () => {
-
-//   };
+  const createCardsProducts = async () => {
+    // loading(true);
+    const { results } = await getAPIs('productGeneral');
+    // loading(false);
+    results.forEach((element) => {
+      // const createObjParam = createObjCard(element.id, element.title, element.thumbnail); 
+      sectionCardProducts.appendChild(createProductItemElement(element));
+    });
+  };
 
 window.onload = () => { 
     selectCategories();
+    createCardsProducts();
   };
